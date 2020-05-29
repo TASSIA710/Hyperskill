7ca79ef30f3de8164de0725ab86019dc416725cf
Tassilo <tassia710@gmail.com>
2020-05-29 22:41:09 +0200

    Linear Equations Solver: Stage 2

--- Java/Linear Equations Solver/Linear Equations Solver/task/src/solver/Main.java
+++ Java/Linear Equations Solver/Linear Equations Solver/task/src/solver/Main.java
@@ -6,14 +6,18 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double a = scanner.nextDouble(), b = scanner.nextDouble();
 
-        double x;
-        // a * x = b;
-        // x = b / a
-        x = b / a;
+        double a = scanner.nextDouble(), b = scanner.nextDouble(), c = scanner.nextDouble();
+        double d = scanner.nextDouble(), e = scanner.nextDouble(), f = scanner.nextDouble();
 
-        System.out.println(x);
+        // (Taken from the task itself):
+        // a*x + b*y = c
+        // y = (f - c*d/a) / (e - b*d/a)
+
+        double y = (f - c*d/a) / (e - b*d/a);
+        double x = (c - b*y) / a;
+
+        System.out.println(x + " " + y);
     }
 
 }
