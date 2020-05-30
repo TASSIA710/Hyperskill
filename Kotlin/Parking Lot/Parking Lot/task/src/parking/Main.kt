4ab01f29b383eb349373f9e01f9977004852441b
Tassilo <tassia710@gmail.com>
2020-05-30 13:56:17 +0200

    Parking Lot: Stage 2

--- Kotlin/Parking Lot/Parking Lot/task/src/parking/Main.kt
+++ Kotlin/Parking Lot/Parking Lot/task/src/parking/Main.kt
@@ -1,7 +1,33 @@
 package parking
 
+import java.util.*
+
+val slots = BooleanArray(10)
+
 fun main() {
-    println("White car has parked.")
-    println("Yellow car left the parking lot.")
-    println("Green car just parked here.")
+    slots[0] = true
+
+    val scanner = Scanner(System.`in`)
+    val action = scanner.next()
+
+    if (action == "park") {
+        val license = scanner.next()
+        val color = scanner.next()
+
+        var slot = 0
+        while (slots[slot]) {
+            slot++
+        }
+
+        slot++;
+        println("$color car parked in spot $slot.")
+    } else if (action == "leave") {
+        val slot = scanner.nextInt()
+        if (slots[slot - 1]) {
+            slots[slot - 1] = false
+            println("Spot $slot is free.")
+        } else {
+            println("There is no car in spot $slot.")
+        }
+    }
 }
